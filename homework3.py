795681dacb6b990d7797c4ad1c851987f682c1cf
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-03-03 22:28:45 +0500

    Create homework3.py

---
+++ homework3.py
@@ -0,0 +1,7 @@
+percent = int(input('Введите число процентов: '))
+if percent == 1:
+    print(percent, 'процент')
+elif percent <=4:
+    print(percent, 'процента')
+else:
+    print(percent, 'процентов')
