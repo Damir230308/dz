bd9e06c687b088a111a47fd0ca0f7ca5dac9b207
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-03-03 22:25:16 +0500

    Delete homework3.py

--- homework3.py
+++
@@ -1,8 +0,0 @@
-# dz3
-percent = int(input('Введите число процентов: '))
-if percent == 1:
-    print(percent, 'процент')
-elif percent <=4:
-    print(percent, 'процента')
-else:
-    print(percent, 'процентов')
