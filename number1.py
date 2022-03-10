05f3612dd05e99b914ee123e6376a1969fface76
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-03-11 00:40:47 +0500

    первый номер домашнего задания на 12 марта 2022 г.

--- number1.py
+++ number1.py
@@ -1,1 +1,29 @@
-# dz
+print('Внимание! Чтобы пользоваться программой необходимо знать английский язык и вводить числа от 1 до 10!')
+number = (input('Введите число, которое хотите перевести (по английски): '))
+
+def num_translate():
+    if number == 'eleven':
+        return None
+    elif number == 'zero':
+            print('ноль')
+    elif number == 'one':
+            print('один')
+    elif number == 'two':
+            print('два')
+    elif number == 'three':
+            print('три')
+    elif number == 'four':
+            print('четыре')
+    elif number == 'five':
+            print('пять')
+    elif number == 'six':
+            print('шесть')
+    elif number == 'seven':
+            print('семь')
+    elif number == 'eight':
+            print('восемь')
+    elif number == 'nine':
+            print('девять')
+    elif number == 'ten':
+            print('десять')
+num_translate()
