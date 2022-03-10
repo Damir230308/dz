137a9eaed0cdc8ed6d8607d4a7125488bb613235
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-03-11 00:41:41 +0500

    третий номер д/з на 12 марта 2022 года

--- number3.py
+++ number3.py
@@ -1,1 +1,10 @@
-# dz
+def thesaurus(*names):
+    result = {}
+    for name in names:
+        key = name[0].capitalize()
+        if key not in result:
+            result[key] = []
+        result[key].append(name)
+    return result
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
