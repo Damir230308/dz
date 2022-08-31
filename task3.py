219549b80b78b66b468df1a76a4c1e9239d975d2
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-08-31 19:40:28 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,5 @@
-# dz
+def reversed_number(number):
+    return str(number) if number < 10 else str(number % 10) + reversed_number(number // 10)
+
+number = int(input("Write u number in this --> : "))
+print(f'Your number: {reversed_number(number)}')
