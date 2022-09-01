e4c82d324e8ead19bfc4239b4d1a55f6277418ab
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-09-01 14:27:32 +0500

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,1 +1,10 @@
-# dz
+def recur(number, i, n, common_sum):
+    if i == n:
+        print(f'Sum of the elements: {common_sum}')
+    if i < n:
+        return recur(i + 1, number / 2 * -1, n, common_sum+number)
+try:
+    n = int(input("Count of elements: "))
+    recur(0, 1, n, 0)
+except ValueError:
+    print("Вы вместо числа ввели строку. Исправьтесь")
