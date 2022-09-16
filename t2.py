5d169176ca125b7ac14375383cb6fc96a79e2e64
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2022-09-16 20:27:25 +0500

    Update t2.py

--- t2.py
+++ t2.py
@@ -8,14 +8,6 @@ def wrapper():
             return n
         else:
             return func_rec(n - 1) + n
-
-    try:
-        n = int(input("Enter a number: "))
-        if func_rec(n) == n * (n + 1) / 2:
-            print('Equality is true')
-    except ValueError:
-        print("You entered the string ((( instead of a number. Correct yourself")
-
 wrapper()
 
 """
