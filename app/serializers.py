b81c4d576388b3ec772d46294ffc84382ddbac1a
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2023-01-31 00:33:10 +0500

    Задание на 6 урок про авторизацию и права
    + к этому скину базу данных где будет токен и доказательство того что авторизация работает

---
+++ app/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import Author, Book, Biography, Article
+
+
+class AuthorModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class BookModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class ArticleModelSerializer(HyperlinkedModelSerializer):
+    author = AuthorModelSerializer()
+
+    class Meta:
+        model = Article
+        fields = '__all__'
