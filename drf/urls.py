99aba12b5cf8730889ad6c1072b8914b093bf764
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2023-02-08 01:55:42 +0500

    Папка drf

---
+++ drf/urls.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet, MyAPIView
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from graphene_django.views import GraphQLView
+
+# Documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Drf',
+        default_version='0.1',
+        description="Doc for project",
+        contact=openapi.Contact(email='bababa@bababa.com'),
+        license=openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
+router.register('book', BookModelViewSet)
+router.register('biography', BiographyModelViewSet)
+router.register('article', ArticleModelViewSet)
+
+urlpatterns = [
+# Default
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
+    path('api/', include(router.urls)),
+
+# GraphQL   
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
+
+# Documentation
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+# Other  
+    #path('api/1/authors', include('app.urls', namespace='1')),
+    #path('api/authors', MyAPIView.as_view()),
+]
+
