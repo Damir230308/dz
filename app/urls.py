bcb3d3c5c36dad6a952a7954f20d7b0a3867ea75
Damir230308 <99240962+Damir230308@users.noreply.github.com>
2023-02-08 01:55:12 +0500

    Папка app

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import MyAPIView
+
+app_name='app'
+urlpatterns = [
+    path('', MyAPIView.as_view())
+]
---
+++ app/views.py
@@ -0,0 +1,35 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Author, Book, Article, Biography
+from .serializers import AuthorModelSerializer, AuthorModelSerializer2, BiographyModelSerializer, BookModelSerializer, ArticleModelSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+
+class BiographyModelViewSet(ModelViewSet):
+    queryset = Biography.objects.all()
+    serializer_class = BiographyModelSerializer
+
+
+class ArticleModelViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleModelSerializer
+
+class MyAPIView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer = AuthorModelSerializer
+    
+    def get_serializer_class(self):
+        if self.request.version == '1':
+            return AuthorModelSerializer
+        return AuthorModelSerializer2
